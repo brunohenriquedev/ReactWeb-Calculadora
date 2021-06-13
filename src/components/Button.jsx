3ebd674322e2df943d65b656d6472bed5b3c344c
Bruno <bruno.henrik.bh@gmail.com>
2021-06-13 13:54:57 -0300

    Projeto Calculadora

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Button.css'
+
+const Button = props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+
+export default Button;
---
+++ src/components/Display.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Display.css'
+
+const Display = props =>
+    <div className="display">{props.value}</div>;
+
+export default Display;
